d29167137f66d88ace72edbd0c10ed96258353f4
Rashid Khan <github.fliplap@spamgourmet.com>
2016-10-18 09:14:12 -0700

    Update to Kibana 5 with timelion in core. Closes #1

--- functions/random.js
+++ functions/random.js
@@ -1,5 +1,6 @@
-var alter = require('../../timelion/server/lib/alter.js');
-var Datasource = require('../../timelion/server/lib/classes/datasource');
+var alter = require('../../../src/core_plugins/timelion/server/lib/alter.js');
+var Datasource = require('../../../src/core_plugins/timelion/server/lib/classes/datasource');
+
 var _ = require('lodash');
 
 module.exports = new Datasource('random', {
